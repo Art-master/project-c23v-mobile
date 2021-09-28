e2145f98e775f1b8e76f85a7a057f7b2d8ca8e6b
Art-_-master <ggn.master@gmail.com>
2021-09-28 22:30:57 +0400

    [FEATURE] add ui-common-resources module

--- app/src/main/java/com/example/c23v/MainActivity.kt
+++ app/src/main/java/com/example/c23v/MainActivity.kt
@@ -3,36 +3,73 @@ package com.example.c23v
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.PathNode
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.c23v.ui.theme.C23vTheme
+import androidx.compose.ui.unit.dp
+import com.example.c23v.ui.theme.ApplicationsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            C23vTheme {
+            ApplicationsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    LoginTextField()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    C23vTheme {
-        Greeting("Android")
+    ApplicationsTheme {
+        Column(modifier = Modifier.padding(16.dp)) {
+            LoginTextField()
+            Spacer(modifier = Modifier.padding(20.dp))
+            PasswordTextField()
+        }
     }
+}
+
+@Composable
+fun LoginTextField() {
+    var text by rememberSaveable { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        placeholder = { Text(text = "placeholder") },
+        leadingIcon = { Icon(Icons.Filled.Favorite, contentDescription = "Localized description") },
+        trailingIcon = { Icon(Icons.Filled.Info, contentDescription = "Localized description") }
+    )
+}
+
+
+@Composable
+fun PasswordTextField() {
+    var text by rememberSaveable { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        placeholder = { Text(text = "placeholder") },
+        leadingIcon = { Icon(Icons.Filled.Favorite, contentDescription = "Localized description") },
+        trailingIcon = { Icon(Icons.Filled.Info, contentDescription = "Localized description") }
+    )
 }
--- app/src/main/java/com/example/c23v/ui/theme/Theme.kt
+++ app/src/main/java/com/example/c23v/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun C23vTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun ApplicationsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
